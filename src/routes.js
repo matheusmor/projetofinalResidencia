1ec869759b8cff534c2c36038cbd8e08e5122e51
matheusmor <matieumor@hotmail.com>
2020-10-03 12:23:26 -0300

    comit

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import { AntDesign } from '@expo/vector-icons'
+
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -14,10 +16,24 @@ const Tab = createBottomTabNavigator();
 
 const Navigation = () => {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}} inicialRouteName="Home" >
+    <Tab.Navigator screenOptions={{headerShown: false}} inicialRouteName="Home"
+    tabBarOptions={{
+      style: {
+          backgroundColor: 'rgba(0, 0, 0, 0.8)',
+          borderTopColor: 'rgba(255, 255, 0.2)',
+      },
+      activeTintColor: '#fff',
+      inactiveTintColor: '#92929c'
+  }} >
 
       <Tab.Screen name="Delete" component={Delete} />
-      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Home" component={Home} 
+      options={{tabBarIcon: ({ color})=> {
+        return(
+        <AntDesign name="home" size={24} color={color} /> );}
+        
+    }}
+      />
       <Tab.Screen name="AddProducts" component={AddProducts} />
 
 
