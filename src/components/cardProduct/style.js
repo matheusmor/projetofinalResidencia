feeef25e72f123fe4333904072329026ca227924
RoResende <68281057+RoResende@users.noreply.github.com>
2020-10-04 15:17:39 -0300

    Consulta a API pronta na Home, falta CSS

--- src/components/cardProduct/style.js
+++ src/components/cardProduct/style.js
@@ -1,9 +1,14 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-
+    width: 155px;
+    height: 180px;
+    justify-content: center;
+    margin: 0px 10px 8px;
 `;
-export const ContainerImg = styled.View`
+export const ContainerImg = styled.Image`
+    height: 90%;
+    width: 100%;
 
 `;
 export const ContainerCont = styled.View`
@@ -18,6 +23,6 @@ export const Name = styled.Text`
 export const Price = styled.Text`
 
 `;
-export const Desc = styled.Text`
+export const Description = styled.Text`
 
 `;
