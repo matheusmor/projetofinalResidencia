54fdec9dd5140d9a15b176bd023dd9ff4381320a
RoResende <68281057+RoResende@users.noreply.github.com>
2020-10-05 08:50:47 -0300

    Imagem padão em caso de erro incluída

--- src/components/cardProduct/style.js
+++ src/components/cardProduct/style.js
@@ -1,17 +1,24 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-    width: 155px;
+    flex: 1;
+    flex-direction: row;
+    width: 90%;
     height: 180px;
     justify-content: center;
     margin: 0px 10px 8px;
 `;
 export const ContainerImg = styled.Image`
+    flex: 1;
     height: 90%;
-    width: 100%;
+    width: 90%;
 
 `;
 export const ContainerCont = styled.View`
+    flex: 1;
+    flex-direction: column;
+    align-content: center;
+    justify-content: center;
 
 `;
 export const ContainerInf = styled.View`
