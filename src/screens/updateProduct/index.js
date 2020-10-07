86194a59652461f3067d352cfd117540ff724a07
RoResende <68281057+RoResende@users.noreply.github.com>
2020-10-07 08:00:05 -0300

    Fixed

--- src/screens/updateProduct/index.js
+++ src/screens/updateProduct/index.js
@@ -1,116 +1,102 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import api from "../../services/api";
+import api from '../../services/api'
+
 import notFound from "../../../assets/images/image-not-found.jpg";
-import SubmitButton from "../../components/submitButton";
+import SubmitButton from '../../components/submitButton'
 
 import {
-  ContainerTop,
-  ContainerBot,
-  ContainerImg,
-  ContainerStock,
-  ContainerInf,
-  ContainerName,
-  ContainerPrice,
-  ContainerDesc,
-  ContainerCategory,
-  Input,
+    ContainerTop,
+    ContainerBot,
+    ContainerImg,
+    ContainerStock,
+    ContainerInf,
+    ContainerName,
+    ContainerPrice,
+    ContainerDesc,
+    ContainerCategory,
+    Input,
 } from "./style";
 
 const Update = ({ route }) => {
-  const [produto, setProduto] = useState();
-  const [categorias, setCategorias] = useState([]);
 
-  const id = route.params?.id ?? 1;
+    const [produto, setProduto] = useState();
+    const [foto, setFoto] = useState();
 
-  useEffect(() => {
-    const handleProduct = async () => {
-      try {
-        const response = await api.get(`/produto/${id}`);
-        const prod = response.data;
+    const id = route.params?.id ?? 1;
 
-        setProduto(prod);
-      } catch (error) {
-        alert("Erro no acesso a API");
-      }
-    };
-    handleProduct();
-    console.log(produto);
-  }, []);
+    useEffect(() => {
+        const handleProduct = async () => {
 
-  useEffect(() => {
-    const handleListCategorias = async () => {
-      try {
-        const response = await api.get("/categoria");
-        const list = response.data;
-        const namesList = [];
-        list.forEach((item) => {
-          namesList.push(item);
-        });
-        setCategorias(namesList);
-      } catch (error) {
-        alert("Erro no acesso a API");
-      }
-    };
-    handleListCategorias();
-  }, []);
 
-  const handleUpdateProduct = async (id) => {
-    try {
-      await api.put(`/produto/${id}`, produto);
-    } catch (error) {
-      alert("Erro no acesso a API");
+            try {
+                const response = await api.get(`/produto/${id}`);
+                const prod = response.data;
+
+                setProduto(prod);
+                setFoto({uri: prod.fotoLink});
+
+            } catch (error) {
+                alert('Erro no acesso a API');
+            }
+        };
+        handleProduct();
+    }, []);
+
+    const handleUpdateProduct = async () => {
+
+        try {
+            await api.put(`/produto/${id}`, produto);
+
+        } catch (error) {
+            alert('Erro no acesso a API');
+        }
+
     }
-  };
 
-  const addDefaultImg = () => {
-    setProduto({ ...produto, fotoLink: { uri: notFound } });
-  };
+    const addDefaultImg = () => {
+        setFoto(notFound);
+    };
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    handleUpdateProduct();
-  };
+    const handleClick = (e) => {
+        handleUpdateProduct();
+    }
 
-  const findCategoria = (id) => {
-    const result = categorias.find((cat) => cat.id === parseInt(id));
-    return result.nome;
-  };
+    return (
+        <>
+            <ContainerTop>
+                <ContainerImg source={foto} onError={addDefaultImg}></ContainerImg>
+                <ContainerInf>
+                    <ContainerName>
+                        <Input placeholder="Nome" value={produto?.nome} 
+                         onChangeText={text => setProduto({ ...produto, nome: text })} />
+                    </ContainerName>
+                    <ContainerPrice>
+                        <Input placeholder="Valor" defaultValue={produto?.valor.toString()} 
+                            keyboardType='numeric'
+                            maxLength={10}
+                            onChangeText={text => setProduto({ ...produto, valor: parseFloat(text) })}/>
+                    </ContainerPrice>
+                    <ContainerDesc>
+                        <Input placeholder="Descrição" value={produto?.descricao}
+                        onChangeText={text => setProduto({ ...produto, descricao: text })} />
+                    </ContainerDesc>
+                </ContainerInf>
+            </ContainerTop>
+            <ContainerBot>
+                <ContainerStock>
+                    <Input placeholder="Estoque" defaultValue={produto?.qtdEstoque.toString()}
+                    keyboardType='numeric'
+                    onChangeText={text => setProduto({ ...produto, qtdEstoque: parseInt(text) })} />
+                </ContainerStock>
+                <ContainerCategory>
+                    <Input placeholder="Categoria" />
+                </ContainerCategory>
+            </ContainerBot>
+            <SubmitButton />
+        </>
+    )
 
-  return (
-    <>
-      <ContainerTop>
-        <ContainerImg
-          source={{ uri: produto?.fotoLink }}
-          onError={addDefaultImg}
-        ></ContainerImg>
-        <ContainerInf>
-          <ContainerName>
-            <Input
-              placeholder={produto?.nome}
-              value={produto?.nome}
-              onChange={(e) => setProduto({ ...produto, nome: e.target.value })}
-            />
-          </ContainerName>
-          <ContainerPrice>
-            <Input placeholder={produto?.valor.toString()} />
-          </ContainerPrice>
-          <ContainerDesc>
-            <Input placeholder="Description" />
-          </ContainerDesc>
-        </ContainerInf>
-      </ContainerTop>
-      <ContainerBot>
-        <ContainerStock>
-          <Input placeholder="Stock" />
-        </ContainerStock>
-        <ContainerCategory>
-          <Input placeholder="Category" />
-        </ContainerCategory>
-      </ContainerBot>
-      <SubmitButton />
-    </>
-  );
-};
+}
 
 export default Update;
