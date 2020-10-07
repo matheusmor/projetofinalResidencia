4bfd1452f5d6b8fcbf6ab809f8706f4dcb0069ce
ErculesM <erculesm10@gmail.com>
2020-10-07 15:05:28 -0300

    Tela de login

--- src/components/cardProduct/style.js
+++ src/components/cardProduct/style.js
@@ -1,7 +1,6 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-  flex: 1;
   flex-direction: row;
   width: 90%;
   height: 180px;
--- src/screens/updateProduct/index.js
+++ src/screens/updateProduct/index.js
@@ -19,85 +19,95 @@ import {
   InputDesc,
   InputCat,
 } from "./style";
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Update = ({ route }) => {
+  const [produto, setProduto] = useState();
+  const [foto, setFoto] = useState();
 
-    const [produto, setProduto] = useState();
-    const [foto, setFoto] = useState();
+  const id = route.params?.id ?? 1;
 
-    const id = route.params?.id ?? 1;
+  useEffect(() => {
+    const handleProduct = async () => {
+      try {
+        const response = await api.get(`/produto/${id}`);
+        const prod = response.data;
 
-    useEffect(() => {
-        const handleProduct = async () => {
-
-
-            try {
-                const response = await api.get(`/produto/${id}`);
-                const prod = response.data;
-
-                setProduto(prod);
-                setFoto({ uri: prod.fotoLink });
-
-            } catch (error) {
-                alert('Erro no acesso a API');
-            }
-        };
-        handleProduct();
-    }, []);
-
-    const handleUpdateProduct = async () => {
-
-        try {
-            await api.put(`/produto/${id}`, produto);
-
-        } catch (error) {
-            alert('Erro no acesso a API');
-        }
-
-    }
-
-    const addDefaultImg = () => {
-        setFoto(notFound);
+        setProduto(prod);
+        setFoto({ uri: prod.fotoLink });
+      } catch (error) {
+        alert("Erro no acesso a API");
+      }
     };
+    handleProduct();
+  }, []);
 
-    return (
-        <>
-            <ContainerTop>
-                <ContainerImg source={foto} onError={addDefaultImg}></ContainerImg>
-                <ContainerInf>
-                    <ContainerName>
-                        <Input placeholder="Nome" value={produto?.nome}
-                            onChangeText={text => setProduto({ ...produto, nome: text })} />
-                    </ContainerName>
-                    <ContainerPrice>
-                        <Input placeholder="Valor" defaultValue={produto?.valor.toString()}
-                            keyboardType='numeric'
-                            maxLength={10}
-                            onChangeText={text => setProduto({ ...produto, valor: parseFloat(text) })} />
-                    </ContainerPrice>
-                    <ContainerDesc>
-                        <Input placeholder="Descrição" value={produto?.descricao}
-                            onChangeText={text => setProduto({ ...produto, descricao: text })} />
-                    </ContainerDesc>
-                </ContainerInf>
-            </ContainerTop>
-            <ContainerBot>
-                <ContainerStock>
-                    <Input placeholder="Estoque" defaultValue={produto?.qtdEstoque.toString()}
-                        keyboardType='numeric'
-                        onChangeText={text => setProduto({ ...produto, qtdEstoque: parseInt(text) })} />
-                </ContainerStock>
-                <ContainerCategory>
-                    <Input placeholder="Categoria" />
-                </ContainerCategory>
-            </ContainerBot>
-            <TouchableOpacity onPress={handleUpdateProduct} >
-                <SubmitButton />
-            </TouchableOpacity>
-        </>
-    )
+  const handleUpdateProduct = async () => {
+    try {
+      await api.put(`/produto/${id}`, produto);
+    } catch (error) {
+      alert("Erro no acesso a API");
+    }
+  };
 
-}
+  const addDefaultImg = () => {
+    setFoto(notFound);
+  };
+
+  return (
+    <>
+      <ContainerTop>
+        <ContainerImg source={foto} onError={addDefaultImg}></ContainerImg>
+        <ContainerInf>
+          <ContainerName>
+            <Input
+              placeholder="Nome"
+              value={produto?.nome}
+              onChangeText={(text) => setProduto({ ...produto, nome: text })}
+            />
+          </ContainerName>
+          <ContainerPrice>
+            <Input
+              placeholder="Valor"
+              defaultValue={produto?.valor.toString()}
+              keyboardType="numeric"
+              maxLength={10}
+              onChangeText={(text) =>
+                setProduto({ ...produto, valor: parseFloat(text) })
+              }
+            />
+          </ContainerPrice>
+          <ContainerDesc>
+            <InputDesc
+              placeholder="Descrição"
+              value={produto?.descricao}
+              onChangeText={(text) =>
+                setProduto({ ...produto, descricao: text })
+              }
+            />
+          </ContainerDesc>
+        </ContainerInf>
+      </ContainerTop>
+      <ContainerBot>
+        <ContainerStock>
+          <Input
+            placeholder="Estoque"
+            defaultValue={produto?.qtdEstoque.toString()}
+            keyboardType="numeric"
+            onChangeText={(text) =>
+              setProduto({ ...produto, qtdEstoque: parseInt(text) })
+            }
+          />
+        </ContainerStock>
+        <ContainerCategory>
+          <InputCat placeholder="Categoria" />
+        </ContainerCategory>
+      </ContainerBot>
+      <TouchableOpacity onPress={handleUpdateProduct}>
+        <SubmitButton />
+      </TouchableOpacity>
+    </>
+  );
+};
 
 export default Update;
