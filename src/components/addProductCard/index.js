375ad4d99ace5fbb903d334372feacc845a0d475
RoResende <68281057+RoResende@users.noreply.github.com>
2020-10-07 14:12:10 -0300

    Adicionar Produtos completo

--- src/components/addProductCard/index.js
+++ src/components/addProductCard/index.js
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Picker } from '@react-native-community/picker';
+
+import prodContext from '../../context'
+import notFound from "../../../assets/images/image-not-found.jpg";
 
 import {
   ContainerTop,
@@ -12,33 +16,50 @@ import {
   ContainerCategory,
   Input,
 } from "./style";
+import { PickerItem } from "react-native/Libraries/Components/Picker/Picker";
 
 const AddProductCard = () => {
+
+  const { produto, setProduto,categorias } = useContext(prodContext);
   return (
+
     <>
       <ContainerTop>
-        <ContainerImg></ContainerImg>
+        <ContainerImg source={notFound}></ContainerImg>
         <ContainerInf>
           <ContainerName>
-            <Input placeholder="Name" />
+            <Input placeholder="Name" defaultValue={produto.nome}
+              onChangeText={text => setProduto({ ...produto, nome: text })} />
           </ContainerName>
           <ContainerPrice>
-            <Input placeholder="Price" />
+            <Input placeholder="Valor" defaultValue={produto?.valor.toString()}
+              keyboardType='numeric'
+              maxLength={10}
+              onChangeText={text => setProduto({ ...produto, valor: parseFloat(text) })} />
           </ContainerPrice>
           <ContainerDesc>
-            <Input placeholder="Description" />
+            <Input placeholder="Descrição" value={produto?.descricao}
+              onChangeText={text => setProduto({ ...produto, descricao: text })} />
           </ContainerDesc>
         </ContainerInf>
       </ContainerTop>
       <ContainerBot>
         <ContainerStock>
-          <Input placeholder="Stock" />
+          <Input placeholder="Estoque" defaultValue={produto?.qtdEstoque.toString()}
+            keyboardType='numeric'
+            onChangeText={text => setProduto({ ...produto, qtdEstoque: parseInt(text) })} />
         </ContainerStock>
         <ContainerCategory>
-          <Input placeholder="Category" />
+          <Picker selectedValue={produto.idCategoria} 
+            onValueChange={itemValue => setProduto({...produto, idCategoria: itemValue})} >
+            {categorias.map(cat => {
+              return <Picker.Item key={cat.id} label={cat.nome} value={cat.id}/>
+            })}
+          </Picker>
         </ContainerCategory>
       </ContainerBot>
     </>
+
   );
 };
 
