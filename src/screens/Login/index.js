69c0b142ca385455560e3d77346e07ba8e16193e
matheusmor <matieumor@hotmail.com>
2020-10-08 14:54:59 -0300

    db

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -6,9 +6,23 @@ import{NavigationContainer} from '@react-navigation/native';
 
 
 const Login= () => {
+  
+  criaTabela();
+  funcao();
+  const validacao=async()=>{
+    // input login input password > select database login e senha if === return true 
+    const id = 0
+const usuario = await Usuario.find(id)
+
+
+
+alert(usuario.login)
+console.log({usuario}, "usuario aqui")
+  }
+
   return (
     <>
-      <Text></Text>
+      <Button title='botao'onPress={validacao} ></Button>
     </>
   );
 };
